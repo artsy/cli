db1e105c5f40a045c9bdff028827eca46e67b79d
Jon Allured <jon.allured@gmail.com>
2019-09-06 15:11:15 -0500

    Get everything configured correctly
    Ya know what I mean?

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
-const fs = require('fs')
-const os = require('os')
+const fs = require("fs")
+const os = require("os")
 
 export const Config = {
   path: (): string => {
     return `${os.homedir()}/.config/artsy`
   },
   readToken: (): string => {
-    return fs.readFileSync(Config.path(), {encoding: 'utf-8'})
+    return fs.readFileSync(Config.path(), { encoding: "utf-8" })
   },
   writeToken: (token: string): void => {
     fs.writeFileSync(Config.path(), token)
-  }
+  },
 }
