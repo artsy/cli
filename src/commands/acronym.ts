c911f17d203d40d3ee1f9a7a28aa3f31d3011836
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2021-02-12 16:47:47 -0500

    feat: add an acronym subcommand

---
+++ src/commands/acronym.ts
@@ -0,0 +1,28 @@
+import { Command, flags } from "@oclif/command"
+import { findAcronym } from "../lib/acronyms"
+
+export default class Acronym extends Command {
+  static description = "Explain an acronym heard at Artsy"
+
+  static examples = [
+    `$ artsy acronym aov
+AOV: Average Order Value`,
+  ]
+
+  static flags = {
+    help: flags.help({ char: "h" }),
+  }
+
+  static args = [{ name: "query" }]
+
+  async run() {
+    const { args } = this.parse(Acronym)
+    const result = findAcronym(args.query)
+
+    if (result) {
+      console.log(result)
+    } else {
+      console.log("Sorry, unknown acronym")
+    }
+  }
+}
