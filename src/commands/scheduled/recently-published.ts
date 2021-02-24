e99002369d9586f756c40bdf52e469566d9bfb07
Steven Hicks <steven.j.hicks@gmail.com>
2021-02-24 10:34:37 -0600

    feat: summarize most recent episode of artsy engineering radio

--- src/commands/scheduled/recently-published.ts
+++ src/commands/scheduled/recently-published.ts
@@ -7,41 +7,115 @@ export default class ScheduledRecentlyPublished extends Command {
     "Describe our most recently published articles and podcast episodes"
 
   async run() {
-    const podcast = await this.getPodcastFeed()
+    const podcast = await this.buildPodcastSummary()
+    const callToAction = this.buildCallToAction()
 
     const response = {
-      blocks: [...podcast],
+      blocks: [...podcast, ...callToAction],
     }
 
     this.log(JSON.stringify(response))
   }
 
-  async getPodcastFeed() {
+  async buildPodcastSummary() {
     const PODCAST_FEED_URL = "https://artsy.github.io/podcast.xml"
 
     const parser = new Parser()
     const feed = await parser.parseURL(PODCAST_FEED_URL)
 
-    const last = feed.items[feed.items.length - 1]
-    const lastPublishedDate = new Date(last.pubDate!).toLocaleDateString(
-      "en-US",
-      {
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-      }
-    )
+    const lastEpisode = feed.items[feed.items.length - 1]
+    const {
+      title,
+      content,
+      pubDate,
+      itunes: { author },
+    } = lastEpisode
+    const formattedDate = new Date(pubDate!).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    })
 
     const blocks = [
       {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: `The last episode of *Artsy Engineering Radio* aired on ${lastPublishedDate}:`,
+          text: `The last episode of *Artsy Engineering Radio* aired on ${formattedDate}:`,
         },
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `${title} | <https://podcasts.apple.com/us/podcast/artsy-engineering-radio/id1545870104|Apple Podcasts> | <https://podcasts.google.com/feed/aHR0cHM6Ly9hcnRzeS5naXRodWIuaW8vcG9kY2FzdC54bWw|Google Podcasts> | <https://open.spotify.com/show/0gJYxpqN6P11dbjNw8VT2a?si=L4TWDrQETwuVO6JR1SOZTQ|Spotify>`,
+        },
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `> ${content}`,
+        },
+      },
+      {
+        type: "divider",
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text:
+            "Have an idea for a blog post or podcast episode? Swing by the #blogging/#engineering-podcast channels anytime. We also have Writing Office Hours every Monday at 2pm ET.",
+        },
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "mrkdwn",
+            text:
+              "<https://artsy.github.io|Artsy Engineering Blog> | Podcast (<https://podcasts.apple.com/us/podcast/artsy-engineering-radio/id1545870104|iTunes>, <https://podcasts.google.com/feed/aHR0cHM6Ly9hcnRzeS5naXRodWIuaW8vcG9kY2FzdC54bWw|Google>)",
+          },
+        ],
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "mrkdwn",
+            text: `${author} - ${formattedDate}`,
+          },
+        ],
       },
     ]
 
     return blocks
+  }
+
+  buildCallToAction() {
+    return [
+      {
+        type: "divider",
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text:
+            "Have an idea for a blog post or podcast episode? Swing by the #blogging/#engineering-podcast channels anytime. We also have Writing Office Hours every Monday at 2pm ET.",
+        },
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "mrkdwn",
+            text:
+              "<https://artsy.github.io|Artsy Engineering> | Podcast (<https://podcasts.apple.com/us/podcast/artsy-engineering-radio/id1545870104|iTunes>, <https://podcasts.google.com/feed/aHR0cHM6Ly9hcnRzeS5naXRodWIuaW8vcG9kY2FzdC54bWw|Google>)",
+          },
+        ],
+      },
+    ]
   }
 }
--- test/commands/scheduled/recently-published.test.ts
+++ test/commands/scheduled/recently-published.test.ts
@@ -10,7 +10,8 @@ describe("scheduled:recently-published", () => {
     .stdout()
     .command(["scheduled:recently-published"])
     .it("shares most recent podcast episode", ctx => {
-      expect(ctx.stdout.trim()).to.include(
+      const response = ctx.stdout.trim()
+      expect(response).to.include(
         JSON.stringify({
           type: "section",
           text: {
@@ -18,6 +19,69 @@ describe("scheduled:recently-published", () => {
             text:
               "The last episode of *Artsy Engineering Radio* aired on Dec 17, 2020:",
           },
+        })
+      )
+      expect(response).to.include(
+        JSON.stringify({
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text:
+              "1: How To Have Good Meetings | <https://podcasts.apple.com/us/podcast/artsy-engineering-radio/id1545870104|Apple Podcasts> | <https://podcasts.google.com/feed/aHR0cHM6Ly9hcnRzeS5naXRodWIuaW8vcG9kY2FzdC54bWw|Google Podcasts> | <https://open.spotify.com/show/0gJYxpqN6P11dbjNw8VT2a?si=L4TWDrQETwuVO6JR1SOZTQ|Spotify>",
+          },
+        })
+      )
+      expect(response).to.include(
+        JSON.stringify({
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text:
+              "> Ash Furrow talks with Steve Hicks about facilitating meaningful and inclusive team meetings, and how meetings are part of building teams, trust, and systems.",
+          },
+        })
+      )
+      expect(response).to.include(
+        JSON.stringify({
+          type: "context",
+          elements: [
+            {
+              type: "mrkdwn",
+              text: "Ash Furrow & Steve Hicks - Dec 17, 2020",
+            },
+          ],
+        })
+      )
+    })
+
+  test
+    .nock("https://artsy.github.io", api =>
+      api.get("/podcast.xml").replyWithFile(200, fixture)
+    )
+    .stdout()
+    .command(["scheduled:recently-published"])
+    .it("lets people know how to contribute", ctx => {
+      const response = ctx.stdout.trim()
+      expect(response).to.include(
+        JSON.stringify({
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text:
+              "Have an idea for a blog post or podcast episode? Swing by the #blogging/#engineering-podcast channels anytime. We also have Writing Office Hours every Monday at 2pm ET.",
+          },
+        })
+      )
+      expect(response).to.include(
+        JSON.stringify({
+          type: "context",
+          elements: [
+            {
+              type: "mrkdwn",
+              text:
+                "<https://artsy.github.io|Artsy Engineering> | Podcast (<https://podcasts.apple.com/us/podcast/artsy-engineering-radio/id1545870104|iTunes>, <https://podcasts.google.com/feed/aHR0cHM6Ly9hcnRzeS5naXRodWIuaW8vcG9kY2FzdC54bWw|Google>)",
+            },
+          ],
         })
       )
     })
