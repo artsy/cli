fdcd67022c75e4b1af665e3db2516a710b09f75f
brainbicycle <brian.beckerle@artsymail.com>
2024-03-22 15:12:16 -0400

    read in query and var file, temp place to store test-queries

--- src/commands/inspect.ts
+++ src/commands/inspect.ts
@@ -1,12 +1,13 @@
 
 import Command from "../base"
 import { Config } from "../config"
+import * as fs from 'fs/promises'
 
 export default class Inspect extends Command {
   static description = "Returns inspection data for a given query"
 
   static examples = [
-    `$ artsy inspect
+    `$ artsy inspect your_query.graphql
       TODO: Put an example here
     `,
   ]
@@ -15,14 +16,33 @@ export default class Inspect extends Command {
     ...Command.flags,
   }
 
-  static args = [{ name: "query", required: true }]
+  static args = [
+    { name: "query-file", required: true },
+    { name: "variables-file", required: true }
+  ]
+
 
   async run() {
     if (!Config.gravityToken())
       this.error("You are not logged in. Run `artsy login`.")
 
     const { args } = this.parse(Inspect)
- 
-    this.log(`hello there`)
+
+    try {
+      const query = await fs.readFile(args['query-file'], 'utf8');
+      let variables = {};
+
+      // If a variables file was provided, read and parse it
+      if (args['variables-file']) {
+        const variablesContent = await fs.readFile(args['variables-file'], 'utf8');
+        variables = JSON.parse(variablesContent); // Parse the JSON variables
+      }
+
+      this.log(`Query:\n${query}`);
+      this.log(`Variables:\n${JSON.stringify(variables, null, 2)}`);
+
+    } catch (error: any) {
+      this.error(`Error reading file: ${error.message}`);
+    }
   }
 }
