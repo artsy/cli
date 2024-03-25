26ae894ac1c300e4b211836301935cbe73d4ea22
Brian Beckerle <49686530+brainbicycle@users.noreply.github.com>
2024-03-25 12:45:23 -0400

    feat: support staging login and gravity env (#310)
    * first pass support staging login and gravity env
    
    * remove unnecessary semicolons, fix lint errors
    
    * more lint issues and prettier
    
    * better solution for logout
    
    * fix tokenKey issue
    
    * fix config prettier issues

--- src/clients/gravity.ts
+++ src/clients/gravity.ts
@@ -2,35 +2,45 @@ import fetch from "node-fetch"
 import { Config } from "../config"
 
 export class Gravity {
-  static BASE_URL = `https://api.artsy.net/`
+  static BASE_URL_PRODUCTION = `https://api.artsy.net/`
+  static BASE_URL_STAGING = `https://stagingapi.artsy.net/`
+
   static REDIRECT_PORT = 27879
 
-  static url(endpoint: string) {
-    return `${Gravity.BASE_URL}${endpoint}`
+  static baseUrl(isStaging = false) {
+    return isStaging ? Gravity.BASE_URL_STAGING : Gravity.BASE_URL_PRODUCTION
   }
 
-  static urls = {
-    current_user: Gravity.url("api/current_user"),
-    auth: Gravity.url("oauth2/authorize"),
-    access_token: Gravity.url("oauth2/access_token"),
-    access_tokens: Gravity.url("api/tokens/access_token"),
-    user_details: Gravity.url("api/current_user"),
-    // tslint:disable-next-line:no-http-string
-    callback: `http://127.0.0.1:${Gravity.REDIRECT_PORT}`,
+  static url(endpoint: string, isStaging = false) {
+    return `${Gravity.baseUrl(isStaging)}${endpoint}`
   }
 
-  static authUrl() {
+  static urls(isStaging = false) {
+    return {
+      current_user: Gravity.url("api/current_user", isStaging),
+      auth: Gravity.url("oauth2/authorize", isStaging),
+      access_token: Gravity.url("oauth2/access_token", isStaging),
+      access_tokens: Gravity.url("api/tokens/access_token", isStaging),
+      user_details: Gravity.url("api/current_user", isStaging),
+      // tslint:disable-next-line:no-http-string
+      callback: `http://127.0.0.1:${Gravity.REDIRECT_PORT}`,
+    }
+  }
+
+  static authUrl(isStaging = false) {
+    const urls = Gravity.urls(isStaging)
     const params = new URLSearchParams()
 
     params.append("client_id", Config.gravityId())
-    params.append("redirect_uri", Gravity.urls.callback)
+    params.append("redirect_uri", urls.callback)
     params.append("response_type", "code")
 
-    const url = `${Gravity.urls.auth}?${params.toString()}`
+    const url = `${urls.auth}?${params.toString()}`
     return url
   }
 
-  static async getAccessToken(code: string) {
+  static async getAccessToken(code: string, isStaging = false) {
+    const urls = Gravity.urls(isStaging)
     const params = new URLSearchParams()
     params.append("code", code.toString())
     params.append("client_id", Config.gravityId())
@@ -38,7 +48,7 @@ export class Gravity {
     params.append("grant_type", "authorization_code")
     params.append("scope", "offline_access")
 
-    const response = await fetch(Gravity.urls.access_token, {
+    const response = await fetch(urls.access_token, {
       method: "POST",
       body: params,
     })
@@ -47,14 +57,13 @@ export class Gravity {
       throw new Error(`${response.status} ${response.statusText}`)
 
     const data = await response.json()
-
     return data
   }
 
-  async get(endpoint: string) {
-    const token: string = Config.gravityToken()
+  async get(endpoint: string, isStaging = false) {
+    const token: string = Config.gravityToken(isStaging)
 
-    const gravityUrl: string = Gravity.url(`api/v1/${endpoint}`)
+    const gravityUrl: string = Gravity.url(`api/v1/${endpoint}`, isStaging)
     const headers = { "X-Access-Token": token }
     const response = await fetch(gravityUrl, { headers })
 
--- src/commands/logout.ts
+++ src/commands/logout.ts
@@ -4,30 +4,47 @@ import { Gravity } from "../clients/gravity"
 import { Config } from "../config"
 
 export default class Logout extends Command {
-  static description = "Expire your local auth token."
+  static description = "Expire your local auth tokens."
 
   static flags = {
     ...Command.flags,
   }
 
   async run() {
-    const token = Config.gravityToken()
+    const prodToken = Config.gravityToken(false)
+    const stagingToken = Config.gravityToken(true)
 
-    if (!token) {
-      this.log("Already logged out!")
-      this.exit()
+    if (!prodToken && !stagingToken) {
+      this.log("Already logged out from both environments!")
+      return
     }
+
+    await this.performLogout(prodToken, false)
+    await this.performLogout(stagingToken, true)
+
+    this.log("Logged out from all environments!")
+  }
+
+  async performLogout(token: string, isStaging: boolean) {
+    if (!token) return
 
     const params = new URLSearchParams()
     params.append("access_token", token)
-    const response = await fetch(Gravity.urls.access_tokens, {
+
+    const response = await fetch(Gravity.urls(isStaging).access_tokens, {
       method: "DELETE",
       body: params,
     })
 
-    if (!response.ok) this.error(`${response.status} ${response.statusText}`)
+    if (!response.ok) {
+      this.error(
+        `Failed to log out from ${
+          isStaging ? "staging" : "production"
+        } environment: ${response.status} ${response.statusText}`
+      )
+    }
 
-    Config.updateConfig({ accessToken: "" })
-    this.log("Logged out!")
+    const tokenKey = isStaging ? "stagingAccessToken" : "accessToken"
+    Config.updateConfig({ [tokenKey]: "" })
   }
 }
